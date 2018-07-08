17eae6f18b899df01f49ec8ca257f6b04250e1d9
Ben Liu <ben196888@gmail.com>
2018-07-08 20:06:40 +1000

    ✨ Introduce argHelper

---
+++ argHelper.js
@@ -0,0 +1,26 @@
+const printOnErrorMsg = () => {
+  console.error('\
+node index.js input output\n\
+Example: node index.js stdin stdout\n\
+         node index.js inputfile.txt stdout\n\
+         node index.js stdin outputfile.txt\n\
+         node index.js inputfile.txt outputfile.txt');
+};
+
+if (process.argv.length !== 4) {
+  printOnErrorMsg();
+  process.exit(1);
+}
+
+function getArgs() {
+  const inputFilename = process.argv[2];
+  const outputFilename = process.argv[3];
+  return {
+    inputFilename,
+    outputFilename,
+  };
+}
+
+module.exports = {
+  getArgs,
+};
