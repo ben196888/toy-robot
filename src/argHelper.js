c174f09cd8dcfd52c618744d75f9476c5ff08d06
Ben Liu <ben196888@gmail.com>
2018-07-15 01:34:27 +1000

    💬 Update error message when process terminated

--- src/argHelper.js
+++ src/argHelper.js
@@ -2,11 +2,11 @@ const log = require('./log');
 
 const printOnErrorMsg = () => {
   log.error('\
-node index.js input output\n\
-Example: node index.js stdin stdout\n\
-         node index.js inputfile.txt stdout\n\
-         node index.js stdin outputfile.txt\n\
-         node index.js inputfile.txt outputfile.txt');
+node src/index.js input output\n\
+Example: node src/index.js stdin stdout\n\
+         node src/index.js inputfile.txt stdout\n\
+         node src/index.js stdin outputfile.txt\n\
+         node src/index.js inputfile.txt outputfile.txt');
 };
 
 if (process.argv.length !== 4) {
