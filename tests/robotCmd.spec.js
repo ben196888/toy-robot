f51d5642fc331e2c02203529ab9edd9209bee511
Ben Liu <ben196888@gmail.com>
2018-07-14 19:08:05 +1000

    ✅ Add unit test for robot validator

--- tests/robotCmd.spec.js
+++ tests/robotCmd.spec.js
@@ -1,9 +1,11 @@
 const chai = require('chai');
+const sinon = require('sinon');
+const log = require('../src/log');
 const robotCmd = require('../src/robotCmd');
 
 const expect = chai.expect;
 
-const { validator } = robotCmd;
+const { validator, robotValidator } = robotCmd;
 
 describe('robotCmd', function() {
   describe('validator', function() {
@@ -19,6 +21,14 @@ describe('robotCmd', function() {
       expect(validator('LEFT')).to.equal(2);
       expect(validator('RIGHT')).to.equal(3);
       expect(validator('REPORT')).to.equal(4);
+    });
+  });
+
+  describe('robotValidator', function() {
+    it('should ignore command and log warning when robot is not placed', function() {
+      const logWarningStub = sinon.stub(log, 'warning');
+      robotValidator();
+      expect(logWarningStub.calledOnceWith('Robot is not placed yet. Ignore commands')).to.be.true;
     });
   });
 });
