446783608bb6dbaf9b6aafbf31930d8121b11e2f
Ben Liu <ben196888@gmail.com>
2018-07-15 14:26:49 +1000

    🎨 Wrapped io stream tests

--- tests/io.spec.js
+++ tests/io.spec.js
@@ -33,25 +33,27 @@ describe('io', function() {
     sandbox.reset();
   });
   describe('init', function() {
-    it('should not called createReadStream when input is stdin', function() {
-      const createReadStreamSpy = sandbox.spy(fs, 'createReadStream');
-      io.init(STDIN, STDOUT);
-      sinon.assert.notCalled(createReadStreamSpy);
-    });
-    it('should call createReadStream once when input is not stdin', function() {
-      const createReadStreamStub = sandbox.stub(fs, 'createReadStream').returns(INPUT_FILE_NAME);
-      io.init(INPUT_FILE_NAME, STDOUT);
-      sinon.assert.calledOnce(createReadStreamStub);
-    });
-    it('should not called createWriteStream when output is stdout', function() {
-      const createWriteStreamSpy = sandbox.spy(fs, 'createWriteStream');
-      io.init(STDIN, STDOUT);
-      sinon.assert.notCalled(createWriteStreamSpy);
-    });
-    it('should call createWriteStream once when output is not stdout', function() {
-      const createWriteStreamStub = sandbox.stub(fs, 'createWriteStream').returns(OUTPUT_FILE_NAME);
-      io.init(STDIN, OUTPUT_FILE_NAME);
-      sinon.assert.calledOnce(createWriteStreamStub);
+    describe('create read and write stream', function() {
+      it('should not called createReadStream when input is stdin', function() {
+        const createReadStreamSpy = sandbox.spy(fs, 'createReadStream');
+        io.init(STDIN, STDOUT);
+        sinon.assert.notCalled(createReadStreamSpy);
+      });
+      it('should call createReadStream once when input is not stdin', function() {
+        const createReadStreamStub = sandbox.stub(fs, 'createReadStream').returns(FAKE_READ_STREAM);
+        io.init(INPUT_FILE_NAME, STDOUT);
+        sinon.assert.calledOnce(createReadStreamStub);
+      });
+      it('should not called createWriteStream when output is stdout', function() {
+        const createWriteStreamSpy = sandbox.spy(fs, 'createWriteStream');
+        io.init(STDIN, STDOUT);
+        sinon.assert.notCalled(createWriteStreamSpy);
+      });
+      it('should call createWriteStream once when output is not stdout', function() {
+        const createWriteStreamStub = sandbox.stub(fs, 'createWriteStream').returns(FAKE_WRITE_STREAM);
+        io.init(STDIN, OUTPUT_FILE_NAME);
+        sinon.assert.calledOnce(createWriteStreamStub);
+      });
     });
   });
 
